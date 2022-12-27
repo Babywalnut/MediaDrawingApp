1b314093e00294bcdf38856f823114877d366081
babywalnut <anfuza@naver.com>
2022-12-27 21:20:46 +0900

    Initial Commit

---
+++ MediaDrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaDrawingApp
+//
+//  Created by 김민성 on 2022/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
